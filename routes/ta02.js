dc4a7f18b09a4c18d08234ac27245de40896a1c5
masterlink0 <stevensonevans@gmail.com>
2020-09-25 16:31:15 -0600

    ta02 done

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,13 +3,78 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req, res, next) => {
-    res.render('pages/ta02', { 
-        title: 'Team Activity 02', 
+const userNames = [];
+
+router.get('/', (req, res, next) => {
+    res.render('pages/ta02', {
+        title: 'Team Activity 02',
         path: '/ta02', // For pug, EJS 
-        activeTA03: true, // For HBS
-        contentCSS: true, // For HBS
+        userNames: userNames,
+        isNotFound: false,
+        duplicate: false
     });
 });
 
-module.exports = router;
+router.post('/addUser', (req, res, next) => {
+    if (userNames.length > 0){
+        userNames.forEach(item => {
+            if (item === req.body.userName) {
+                res.render('pages/ta02', {
+                    title: 'Team Activity 02',
+                    path: '/ta02',
+                    userNames: userNames,
+                    isNotFound: false,
+                    duplicate: true
+                })
+            }
+            else {
+                userNames.push(req.body.userName);
+                res.render('pages/ta02', {
+                    title: 'Team Activity 02',
+                    path: '/ta02',
+                    userNames: userNames,
+                    isNotFound: false,
+                    duplicate: false
+                })
+            }
+        });
+    }
+    else{
+        userNames.push(req.body.userName);
+        res.render('pages/ta02', {
+            title: 'Team Activity 02',
+            path: '/ta02',
+            userNames: userNames,
+            isNotFound: false,
+            duplicate: false
+        }) 
+    }
+})
+
+    router.post('/removeUser', (req, res, next) => {
+        userNames.forEach(item => {
+            if (item === req.body.removeName) {
+                let postion = userNames.findIndex((item) => item === req.body.removeName);
+                userNames.splice(postion, 1)
+                res.render('pages/ta02', {
+                    title: 'Team Activity 02',
+                    path: '/ta02',
+                    userNames: userNames,
+                    isNotFound: false,
+                    duplicate: false
+                })
+            }
+            else {
+                res.render('pages/ta02', {
+                    title: 'Team Activity 02',
+                    path: '/ta02',
+                    userNames: userNames,
+                    isNotFound: true,
+                    duplicate: false
+                })
+            }
+        })
+
+    });
+
+    module.exports = router;

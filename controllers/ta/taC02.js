d3e67687f83ea9cdb179eea86fb11206bf2d582d
masterlink0 <stevensonevans@gmail.com>
2020-10-03 21:24:37 -0600

    fix heroku

--- controllers/ta/taC02.js
+++ controllers/ta/taC02.js
@@ -1,6 +1,6 @@
 //here
 const userNames = [];
-const { post } = require("request");
+//const { post } = require("request");
 
 exports.getTa02 = (req, res, next) => {
     res.render('../views/pages/ta02', {

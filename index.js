f6f787e126bab7a44e712e32ac824108a77db51c
masterlink0 <stevensonevans@gmail.com>
2020-10-10 10:14:13 -0600

    I dont even know anymore

--- index.js
+++ index.js
@@ -8,43 +8,44 @@ const app = express();
 const routes = require('./routes') //note: express auto adds index.js
 const errorController = require('./controllers/404');
 
-const { mongoose } = require('mongoose');
-const mongooseConnect = require(mongoose);
+const mongoose = require('mongoose'); //this changed
+
 
 
 const cors = require('cors'); // Place this with other requires (like 'path' and 'express')
 
 const corsOptions = {
-    origin: "https://cse341-node.herokuapp.com/",
-    optionsSuccessStatus: 200
+  origin: "https://cse341-node.herokuapp.com/",
+  optionsSuccessStatus: 200
 };
 
 app.use(cors(corsOptions));
 
 const options = {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    family: 4
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  family: 4
 };
 
-const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://masterlink0:thisisevanspassword@cse341shop.mud7h.mongodb.net/<dbname>?retryWrites=true&w=majority";
-                        
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://masterlink0:thisisevanspassword@cse341shop.mud7h.mongodb.net/shop?retryWrites=true&w=majority";
+
 
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(bodyParser({extended: false})); // For parsing the body of a POST
+app.use(bodyParser({ extended: false })); // For parsing the body of a POST
 //app.get('/', routes);
 app.use('/', routes);
 app.use(errorController.get404);
 
-mongooseConnect.connect("mongodb+srv://masterlink0:thisisevanspassword@cse341shop.mud7h.mongodb.net/shop?retryWrites=true&w=majority", options);
-mongooseConnect.then(result => {
-     // This should be your user handling code implement following the course videos
-     app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+mongoose
+  .connect(MONGODB_URL, options)
+  .then(result => {
+    // This should be your user handling code implement following the course videos
+    app.listen(PORT, () => console.log(`Listening on ${PORT}`));
   })
   .catch(err => {
     console.log(err);
